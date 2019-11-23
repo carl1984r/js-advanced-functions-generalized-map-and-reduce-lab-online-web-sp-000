b5af47943d20b9cd4eb96e856832ba09e0f059c9
Bry Cox <carl.cc@gmail.com>
2019-11-23 00:16:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ function map(arr, fx) {
   };
   return mappedArr;
 };
+
+function reduce(arr, fx, accumulator) {
+  let i = 0;
+  if(!accumulator) {
+    accumulator = arr[0];
+    i = 1;
+  };
+  for(; i < arr.length; i++) {
+    accumulator = fx(arr[i], accumulator);
+  };
+  return accumulator;
+};
